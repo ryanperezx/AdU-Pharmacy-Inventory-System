3bed1de8b1e17d26c37d01d3a66a3907c964ca10
ryanperez473 <ryanperez473@gmail.com>
2018-11-07 12:17:59 +0800

    Functions for add subjects

--- AdU Pharmacy Inventory System/Academic Management/AddSubject.xaml.cs
+++ AdU Pharmacy Inventory System/Academic Management/AddSubject.xaml.cs
@@ -8,10 +8,8 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Data.SqlServerCe;
+
 
 namespace AdU_Pharmacy_Inventory_System
 {
@@ -27,7 +25,137 @@ namespace AdU_Pharmacy_Inventory_System
 
         private void searchCode_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
+            if (string.IsNullOrEmpty(txtSubjCode.Text))
+            {
+                MessageBox.Show("Subject code field is empty!");
+            }
+            else
+            {
+                SqlCeConnection conn = DBUtils.GetDBConnection();
+                conn.Open();
+                using (SqlCeCommand cmd = new SqlCeCommand("Select COUNT(1) from Subjects where subjCode = @subjCode", conn))
+                {
+                    cmd.Parameters.AddWithValue("@subjCode", txtSubjCode.Text);
+                    int subjCount;
+                    subjCount = (int)cmd.ExecuteScalar();
+                    if (subjCount > 0)
+                    {
+                        string subjCode = txtSubjCode.Text;
+                        using (SqlCeCommand cmd1 = new SqlCeCommand("SELECT * from Subjects where subjCode = @subjCode",conn))
+                        {
+                            cmd1.Parameters.AddWithValue("@subjCode", subjCode);
+                            using(SqlCeDataReader reader = cmd1.ExecuteResultSet(ResultSetOptions.Scrollable))
+                            {
+                                if (reader.HasRows)
+                                {
+                                    reader.Read();
+                                    //1
+                                    int subjNameIndex = reader.GetOrdinal("subjName");
+                                    string subjName = Convert.ToString(reader.GetValue(subjNameIndex));
 
+                                    txtSubjName.Text = subjName;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Subject does not exist or is not in the database!");
+                    }
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtSubjCode.Text) || string.IsNullOrEmpty(txtSubjName.Text))
+            {
+                MessageBox.Show("One or more fields are empty!");
+            }
+            else
+            {
+                string sMessageBoxText = "Are all fields correct?";
+                string sCaption = "Add Subject";
+                MessageBoxButton btnMessageBox = MessageBoxButton.YesNoCancel;
+                MessageBoxImage icnMessageBox = MessageBoxImage.Warning;
+
+                MessageBoxResult dr = MessageBox.Show(sMessageBoxText, sCaption, btnMessageBox, icnMessageBox);
+                switch (dr)
+                {
+                    case MessageBoxResult.Yes:
+                        SqlCeConnection conn = DBUtils.GetDBConnection();
+                        conn.Open();
+
+                        string subjCode = txtSubjCode.Text;
+                        string subjName = txtSubjName.Text;
+
+                        using (SqlCeCommand cmd = new SqlCeCommand("INSERT into Subjects (subjCode, subjName) VALUES (@subjCode, @subjName)", conn))
+                        {
+                            cmd.Parameters.AddWithValue("@subjCode", subjCode);
+                            cmd.Parameters.AddWithValue("@subjName", subjName);
+
+                            try
+                            {
+                                cmd.ExecuteNonQuery();
+                                MessageBox.Show("Subject Added!");
+                                emptyTextbox();
+                            }
+                            catch (SqlCeException ex)
+                            {
+                                MessageBox.Show("Error! Log has been updated with the error.");
+                            }
+                        }
+                        break;
+                    case MessageBoxResult.No: break;
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSubjCode.Text) || string.IsNullOrEmpty(txtSubjName.Text))
+            {
+                MessageBox.Show("One or more fields are empty!");
+            }
+            else
+            {
+                string sMessageBoxText = "Do you want to delete the subject?";
+                string sCaption = "Delete Subject";
+                MessageBoxButton btnMessageBox = MessageBoxButton.YesNoCancel;
+                MessageBoxImage icnMessageBox = MessageBoxImage.Warning;
+
+                MessageBoxResult dr = MessageBox.Show(sMessageBoxText, sCaption, btnMessageBox, icnMessageBox);
+                switch (dr)
+                {
+                    case MessageBoxResult.Yes:
+                        SqlCeConnection conn = DBUtils.GetDBConnection();
+                        conn.Open();
+                        using (SqlCeCommand command = new SqlCeCommand("DELETE from Subjects where subjCode= @subjCode", conn))
+                        {
+                            command.Parameters.AddWithValue("@subjCode", txtSubjCode.Text);
+                            int result = command.ExecuteNonQuery();
+                            if (result == 1)
+                            {
+                                MessageBox.Show("Subject has been deleted!");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Subject does not exist!");
+                                return;
+                            }
+                        }
+                        emptyTextbox();
+                        break;
+
+                    case MessageBoxResult.No: break;
+                }
+            }
+            }
+
+        private void emptyTextbox()
+        {
+            txtSubjCode.Text = null;
+            txtSubjName.Text = null;
         }
     }
 }
--- AdU Pharmacy Inventory System/Stock Management/Add Inventory.xaml.cs
+++ AdU Pharmacy Inventory System/Stock Management/Add Inventory.xaml.cs
@@ -5,14 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.Data.SqlServerCe;
+using System.Data.SqlClient;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using System.Text.RegularExpressions;
+using System.Data.Common;
 namespace AdU_Pharmacy_Inventory_System
 {
     /// <summary>
@@ -23,6 +20,201 @@ namespace AdU_Pharmacy_Inventory_System
         public Add_Inventory()
         {
             InitializeComponent();
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInventName.Text))
+            {
+                MessageBox.Show("Inventory name field is empty!");
+                txtInventName.Focus();
+            }
+            else
+            {
+                SqlCeConnection conn = DBUtils.GetDBConnection();
+                conn.Open();
+                
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(cmbInventType.Text) || string.IsNullOrEmpty(txtInventName.Text) || string.IsNullOrEmpty(txtQty.Text) || string.IsNullOrEmpty(txtSize.Text) || string.IsNullOrEmpty(cmbUnit.Text))
+            {
+                MessageBox.Show("One or more fields are empty!");
+            }
+            else
+            {
+                string sMessageBoxText = "Are all fields correct?";
+                string sCaption = "Add Inventory";
+                MessageBoxButton btnMessageBox = MessageBoxButton.YesNoCancel;
+                MessageBoxImage icnMessageBox = MessageBoxImage.Warning;
+
+                MessageBoxResult dr = MessageBox.Show(sMessageBoxText, sCaption, btnMessageBox, icnMessageBox);
+                switch (dr)
+                {
+                    case MessageBoxResult.Yes:
+                        SqlCeConnection conn = DBUtils.GetDBConnection();
+                        conn.Open();
+                        using (SqlCeCommand cmd = new SqlCeCommand("INSERT into InventoryStock (inventType, name, qty, size, unit, remarks) VALUES (@inventType, @inventName, @qty, @size, @unit, @remarks)", conn))
+                        {
+                            cmd.Parameters.AddWithValue("@inventType", cmbInventType.Text);
+                            cmd.Parameters.AddWithValue("@inventName", txtInventName.Text);
+                            cmd.Parameters.AddWithValue("@qty", txtQty.Text);
+                            cmd.Parameters.AddWithValue("@size", txtSize.Text);
+                            cmd.Parameters.AddWithValue("@unit", cmbUnit.Text);
+                            cmd.Parameters.AddWithValue("@remarks", txtRemarks.Text);
+
+                            try
+                            {
+                                cmd.ExecuteNonQuery();
+                                MessageBox.Show("Added Successfully");
+                            }
+                            catch (SqlCeException ex)
+                            {
+                                MessageBox.Show("Error! Log has been updated with the error.");
+                            }
+                        }
+                        break;
+                    case MessageBoxResult.No: break;
+
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInventName.Text))
+            {
+                MessageBox.Show("Inventory name field is empty!");
+                txtInventName.Focus();
+            }
+            else
+            {
+                string sMessageBoxText = "Do you want to delete this record?";
+                string sCaption = "Delete Inventory record";
+                MessageBoxButton btnMessageBox = MessageBoxButton.YesNoCancel;
+                MessageBoxImage icnMessageBox = MessageBoxImage.Warning;
+
+                MessageBoxResult dr = MessageBox.Show(sMessageBoxText, sCaption, btnMessageBox, icnMessageBox);
+                switch (dr)
+                {
+                    case MessageBoxResult.Yes:
+                        SqlCeConnection conn = DBUtils.GetDBConnection();
+                        conn.Open();
+                        using(SqlCeCommand cmd = new SqlCeCommand("SELECT COUNT(1) from inventoryStock where name = @inventName", conn))
+                        {
+                            cmd.Parameters.AddWithValue("@inventName", txtInventName.Text);
+                            int count = (int)cmd.ExecuteScalar();
+                            if(count > 0)
+                            {
+
+                            }
+                            else
+                            {
+
+
+                            }
+                        }
+                        break;
+                    case MessageBoxResult.No:
+                        break;
+                }
+            }
+        }
+
+        private void txtQty_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+
+        private void searchInName_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInventName.Text))
+            {
+                MessageBox.Show("Inventory Name field is empty!");
+            }
+            else
+            {
+                SqlCeConnection conn = DBUtils.GetDBConnection();
+                conn.Open();
+                using (SqlCeCommand cmd = new SqlCeCommand("SELECT COUNT(1) from inventoryStock where name = @inventName", conn))
+                {
+                    cmd.Parameters.AddWithValue("@inventName", txtInventName.Text);
+                    int count = (int)cmd.ExecuteScalar();
+                    if (count > 0)
+                    {
+                        using (SqlCeCommand cmd1 = new SqlCeCommand("SELECT * from inventoryStock where name = @inventName", conn))
+                        {
+                            cmd1.Parameters.AddWithValue("@inventName", txtInventName.Text);
+                            using (DbDataReader reader = cmd1.ExecuteResultSet(ResultSetOptions.Scrollable))
+                            {
+                                if (reader.HasRows)
+                                {
+                                    reader.Read();
+
+                                    string inventType = Convert.ToString(reader.GetValue(1));
+
+                                    int qtyIndex = reader.GetOrdinal("qty");
+                                    int qty = Convert.ToInt32(reader.GetValue(qtyIndex));
+ 
+                                    int sizeIndex = reader.GetOrdinal("size");
+                                    string size = Convert.ToString(reader.GetValue(sizeIndex));
+
+                                    int unitIndex = reader.GetOrdinal("unit");
+                                    string unit = Convert.ToString(reader.GetValue(unitIndex));
+
+                                    int remarksIndex = reader.GetOrdinal("remarks");
+                                    string remarks = Convert.ToString(reader.GetValue(remarksIndex));
+
+                                    cmbInventType.Text = inventType;
+                                    txtQty.Text = qty.ToString();
+                                    txtSize.Text = size;
+                                    cmbUnit.Text = unit;
+                                    txtRemarks.Text = remarks;
+
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Item does not exist!");
+                        emptyFields();
+                    }
+                }
+            }
+        }
+
+        private void emptyFields()
+        {
+            cmbInventType.SelectedIndex = -1;
+            txtInventName.Text = null;
+            txtQty.Text = null;
+            txtSize.Text = null;
+            cmbUnit.SelectedIndex = -1;
+            txtRemarks.Text = null;
+        }
+
+        private void disableFields()
+        {
+            cmbInventType.IsEnabled = false;
+            cmbUnit.IsEnabled = false;
+
+            txtQty.IsReadOnly = true;
+            txtSize.IsReadOnly = true;
+            txtRemarks.IsReadOnly = true;
+        }
+        
+        private void enableFields()
+        {
+            cmbInventType.IsEnabled = true;
+            cmbUnit.IsEnabled = true;
+
+            txtQty.IsReadOnly = false;
+            txtSize.IsReadOnly = false;
+            txtRemarks.IsReadOnly = false;
         }
     }
 }
