65555edccd33cd5dc1cd24df67329c6678783001
ryanperez473 <ryanperez473@gmail.com>
2018-11-08 23:07:14 +0800

    Added function for Apparatus Stock out 1/2

--- AdU Pharmacy Inventory System/Stock Management/ApparatusStockOut.xaml.cs
+++ AdU Pharmacy Inventory System/Stock Management/ApparatusStockOut.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlServerCe;
+using System.Data.SqlClient;
+using System.Data.Common;
 
 namespace AdU_Pharmacy_Inventory_System
 {
@@ -23,6 +26,38 @@ namespace AdU_Pharmacy_Inventory_System
         public ApparatusStockOut()
         {
             InitializeComponent();
+            fillInventory();
+        }
+
+        private void fillInventory()
+        {
+            SqlCeConnection conn = DBUtils.GetDBConnection();
+            conn.Open();
+            using (SqlCeCommand cmd = new SqlCeCommand("SELECT name from inventoryStock",conn))
+            {
+                using (DbDataReader reader = cmd.ExecuteResultSet(ResultSetOptions.Scrollable))
+                {
+                    if (reader.HasRows)
+                    {
+                        cmbAppaName.Items.Clear();
+                        while (reader.Read())
+                        {
+                            string appaName = reader["name"].ToString();
+                            cmbAppaName.Items.Add(appaName);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnStockOut_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
