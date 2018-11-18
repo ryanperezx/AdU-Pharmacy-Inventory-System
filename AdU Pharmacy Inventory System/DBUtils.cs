8653c5313c85683e6edb8fff2832fef1c08df2f1
ryanperez473 <ryanperez473@gmail.com>
2018-11-19 01:39:25 +0800

    Added functions for login (but currently commented out for development purposes) Added functions for forgot password

--- AdU Pharmacy Inventory System/DBUtils.cs
+++ AdU Pharmacy Inventory System/DBUtils.cs
@@ -10,6 +10,7 @@ namespace AdU_Pharmacy_Inventory_System
         {
             string user = Environment.UserName;
             string datasource = @"C:\Users\" + user + @"\source\repos\ryanperezx\AdU-Pharmacy-Inventory-System\AdU Pharmacy Inventory System\bin\Debug\inventSystemDB.sdf";
+            //string datasource = @"C:\Users\" + user + @"\Desktop\aduph-is\inventSystemDB.sdf";
             return DBSQLServerUtils.GetDBConnection(datasource);
         }
     }
